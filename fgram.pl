dc24ee9cebbb5d9826c71e1a07b746b6d31a36ab
Aman Kansal <amankansal.india@gmail.com>
2018-09-19 23:49:39 +0530

    using dcg

--- fgram.pl
+++ fgram.pl
@@ -1,9 +1,19 @@
-s(X,Z):- np(X,Y),vp(Y,Z).
-np(X,Z):- det(X,Y),n(Y,Z).
-vp(X,Z):-v(X,Y),np(Y,Z).
-vp(Z,A):-v(Z,A).
-det([a|W],W).
-det([the|W],W).
-n([woman|W],W).
-n([man|W],W).
-v([shoots|W],W).
+% s(X,Z):- np(X,Y),vp(Y,Z).
+% np(X,Z):- det(X,Y),n(Y,Z).
+% vp(X,Z):-v(X,Y),np(Y,Z).
+% vp(Z,A):-v(Z,A).
+% det([a|W],W).
+% det([the|W],W).
+% n([woman|W],W).
+% n([man|W],W).
+% v([shoots|W],W).
+
+s1 --> np,vp.
+np --> det,n.
+vp --> v,np.
+vp --> v.
+det --> [the].
+det --> [a].
+n --> [woman].
+n --> [man].
+v --> [shoots].
